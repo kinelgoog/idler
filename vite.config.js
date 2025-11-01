a726990f54a10438c4ca5e5a01f31e368d571266
kinelgoog <kinelgoog@gmail.com>
2025-11-02 02:05:49 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,10 @@ export default defineConfig({
   },
   preview: {
     host: true,
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: [
+      'idler-pil7.onrender.com',
+      '.onrender.com' // разрешает все поддомены onrender.com
+    ]
   }
 })
